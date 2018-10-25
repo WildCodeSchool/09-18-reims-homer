148f26dcddeb3e54ef32d2c0d611ee739532515b
Michaël BOURMEAU <michael.bourmeau@gmail.com>
2018-10-25 15:54:32 +0200

    formulaire

--- front/react_quete/src/SignUp.js
+++ front/react_quete/src/SignUp.js
@@ -5,19 +5,61 @@ class SignUp extends Component {
     super();
     this.state = {
       email: "",
+      password: "",
+      passwordbis: "",
+      name: "",
+      lastname: "",
     };
     this.updateEmailField = this.updateEmailField.bind(this);
+    this.updatePassword = this.updatePassword.bind(this);
+    this.updatePasswordbis = this.updatePasswordbis.bind(this);
+    this.updateName = this.updateName.bind(this);
+    this.updateLastname = this.updateLastname.bind(this);
   }
 
   updateEmailField(event) {
     this.setState({ email: event.target.value });
   }
 
+  updatePassword(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  updatePasswordbis(event) {
+    this.setState({ passwordbis: event.target.value });
+  }
+
+  updateName(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  updateLastname(event) {
+    this.setState({ lastname: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
   render() {
+    const myJSON = JSON.stringify(this.state, 1, 1);
     return (
       <div>
-        <h1>{this.state.email}</h1>
-        <input onChange={this.updateEmailField} type="email" name="email" />
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          <h1>{myJSON}</h1>
+          <input onChange={this.updateEmailField} type="email" name="email" />
+          <br />
+          <input onChange={this.updatePassword} type="text" />
+          <br />
+          <input onChange={this.updatePasswordbis} type="text" />
+          <br />
+          <input onChange={this.updateName} type="text" />
+          <br />
+          <input onChange={this.updateLastname} type="text" />
+          <br />
+          <input type="submit" value="Soumettre" />
+        </form>
       </div>
     );
   }
