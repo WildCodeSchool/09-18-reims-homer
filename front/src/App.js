7ba3fca5d746bd430dd5027ff206e95d51c0f6cd
Fabien Raymond <voortexxx@gmail.com>
2018-10-25 15:04:28 +0200

    Transfer state  into App.js

--- front/src/App.js
+++ front/src/App.js
@@ -4,10 +4,25 @@ import "./App.css";
 import SignUp from "./SignUp";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: ""
+    };
+    this.updateEmailField = this.updateEmailField.bind(this);
+  }
+  updateEmailField(event) {
+    this.setState({
+      email: event.target.value
+    });
+  }
   render() {
     return (
       <div className="App">
-        <SignUp />
+        <SignUp
+          updateEmailField={this.updateEmailField}
+          email={this.state.email}
+        />
       </div>
     );
   }
--- front/src/SignUp.js
+++ front/src/SignUp.js
@@ -1,24 +1,13 @@
 import React, { Component } from "react";
 
 class SignUp extends Component {
-  constructor() {
-    super();
-    this.state = {
-      email: ""
-    };
-    this.updateEmailField = this.updateEmailField.bind(this);
-  }
-  updateEmailField(event) {
-    this.setState({
-      email: event.target.value
-    });
-  }
   render() {
+    const { email, updateEmailField } = { ...this.props };
     return (
       <div>
-        <h1>{this.state.email}</h1>
+        <h1>{email}</h1>
         <input
-          onChange={event => this.updateEmailField(event)}
+          onChange={event => updateEmailField(event)}
           type="email"
           name="email"
         />
