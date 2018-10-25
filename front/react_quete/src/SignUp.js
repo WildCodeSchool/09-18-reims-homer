749ca217104ec08338a7c15de0267919dd811355
Michaël BOURMEAU <michael.bourmeau@gmail.com>
2018-10-25 14:29:28 +0200

    SignUp component

---
+++ front/react_quete/src/SignUp.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+class SignUp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+    };
+    this.updateEmailField = this.updateEmailField.bind(this);
+  }
+
+  updateEmailField(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.email}</h1>
+        <input onChange={this.updateEmailField} type="email" name="email" />
+      </div>
+    );
+  }
+}
+
+export default SignUp;
