3da999fcc0192fb02408819f3d75b7015f06de87
Fabien Raymond <voortexxx@gmail.com>
2018-12-03 17:15:32 +0100

    connect front with back

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -10,12 +10,8 @@ router.post("/signup", function(req, res, next) {
   const formData = req.body;
   console.log(formData);
   connection.query("INSERT INTO users SET ?", formData, err => {
-    if (err) {
-      console.log(err);
-      res.status(500).send("Erreur lors de la création d'un utilisateur");
-    } else {
-      res.sendStatus(200);
-    }
+    if (err) res.status(500).json({ flash: err.message });
+    else res.status(200).json({ flash: "User has been signed up !" });
   });
 });
 
