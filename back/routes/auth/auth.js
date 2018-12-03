c19f0b0f4afb27e40ef09edd1c905eaf4dcda187
Fabien Raymond <voortexxx@gmail.com>
2018-12-03 16:12:49 +0100

    add post functionality

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,8 +1,22 @@
 var express = require("express");
 var router = express.Router();
+const connection = require("../../helpers/db.js");
+const bodyParser = require("body-parser");
+
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
 
 router.post("/signup", function(req, res, next) {
-  res.send("I am in POST signup");
+  const formData = req.body;
+  console.log(formData);
+  connection.query("INSERT INTO users SET ?", formData, err => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Erreur lors de la création d'un utilisateur");
+    } else {
+      res.sendStatus(200);
+    }
+  });
 });
 
 module.exports = router;
