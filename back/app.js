ac5c70b620ed8a091a503a1acca20f037ba24208
Robin Guerre <robin.guerre@yahoo.fr>
2018-12-03 17:58:31 +0100

    auth.js exported

--- back/app.js
+++ back/app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const app = express();
+const authRouter = require('./routes/auth/auth.js');
 
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,6 +14,9 @@ app.use(express.static(__dirname + "/public"));
 app.get("/", (req, res) => {
   res.send("youhou");
 });
+
+app.use("/auth", authRouter);
+
 app.use(function(req, res, next) {
   var err = new Error("Not Found");
   err.status = 404;
