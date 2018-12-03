60a8bcda38b5f06f39df9c32ccd9020da1652f44
Michaël BOURMEU <michael.bourmeau@outlook.fr>
2018-12-03 17:32:02 +0100

    front

--- front/src/SignUp.js
+++ front/src/SignUp.js
@@ -4,55 +4,65 @@ class SignUp extends Component {
   constructor() {
     super();
     this.state = {
-      email: "",
-      password: "",
-      passwordbis: "",
-      name: "",
-      lastname: "",
+      auth: {
+        email: "",
+        password: "",
+        name: "",
+        lastname: ""
+      },
+      flash: ""
     };
     this.updateEmailField = this.updateEmailField.bind(this);
     this.updatePassword = this.updatePassword.bind(this);
-    this.updatePasswordbis = this.updatePasswordbis.bind(this);
     this.updateName = this.updateName.bind(this);
     this.updateLastname = this.updateLastname.bind(this);
   }
 
   updateEmailField(event) {
-    this.setState({ email: event.target.value });
+    this.setState({ auth: { ...this.state.auth, email: event.target.value } });
   }
 
   updatePassword(event) {
-    this.setState({ password: event.target.value });
-  }
-
-  updatePasswordbis(event) {
-    this.setState({ passwordbis: event.target.value });
+    this.setState({
+      auth: { ...this.state.auth, password: event.target.value }
+    });
   }
 
   updateName(event) {
-    this.setState({ name: event.target.value });
+    this.setState({ auth: { ...this.state.auth, name: event.target.value } });
   }
 
   updateLastname(event) {
-    this.setState({ lastname: event.target.value });
+    this.setState({
+      auth: { ...this.state.auth, lastname: event.target.value }
+    });
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    console.log(this.state);
+    fetch("/auth/signup", {
+      method: "POST",
+      headers: new Headers({
+        "Content-Type": "application/json"
+      }),
+      body: JSON.stringify(this.state.auth)
+    })
+      .then(res => res.json())
+      .then(
+        res => this.setState({ flash: res.flash }),
+        err => this.setState({ flash: err.flash })
+      );
   }
 
   render() {
-    const myJSON = JSON.stringify(this.state, 1, 1);
+    const myJSON = JSON.stringify(this.state.auth, 1, 1);
     return (
       <div>
-        <form onSubmit={(event) => this.handleSubmit(event)}>
+        <form onSubmit={event => this.handleSubmit(event)}>
           <h1>{myJSON}</h1>
           <input onChange={this.updateEmailField} type="email" name="email" />
           <br />
           <input onChange={this.updatePassword} type="text" />
-          <br />
-          <input onChange={this.updatePasswordbis} type="text" />
           <br />
           <input onChange={this.updateName} type="text" />
           <br />
@@ -60,6 +70,7 @@ class SignUp extends Component {
           <br />
           <input type="submit" value="Soumettre" />
         </form>
+        <h2>{this.state.flash}</h2>
       </div>
     );
   }
