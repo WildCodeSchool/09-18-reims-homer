3663a3676b1eda847670b9744c826510fa19c038
Florentin C <florentin.coyard@gmail.com>
2018-12-03 11:53:05 +0100

    added auth/signup route

---
+++ back/routes/auth/auth.js
@@ -0,0 +1,8 @@
+const express = require("express");
+var router = express.Router();
+
+router.post('/signup', function (req, res, next) {
+  res.send('I am in POST signup')
+})
+
+module.exports = router;
