33d3368361c12249a94e83217012b1df82ec2955
CharlesC51 <c.crepin@net-c.fr>
2018-12-06 10:12:37 +0100

    odyssey quest 5

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
+const connection = require("../helpers/db");
+
 router.post("/signup", function(req, res, next) {
-  res.send("I am in POST signup");
+  connection.query(
+    "INSERT INTO odysseyHomer(email, password, name, lastname) VALUES(?,?,?,?)",
+    [req.body.email, req.body.password, req.body.name, req.body.lastname],
+    function(error, results, fields) {
+      if (error) {
+        res.status(500).json({ flash: error.message });
+      }
+      res.status(200).json({ flash: "You are signed up !" });
+    }
+  );
 });
 
 module.exports = router;
