a88df1e149aa1f4441d70a92fab523e98819d1bf
Michel <descotesmichel@aol.com>
2018-12-07 18:54:54 +0100

    creation dossier front

--- front/homer-odyssey/src/App.js
+++ front/homer-odyssey/src/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
-import Email from "./SignUp";
-import { Container } from "reactstrap";
 
 class App extends Component {
   render() {
     return (
-      <Container>
-        <header className="text-center">Homer Odyssey</header>
-
-        <Email />
-      </Container>
+      <div>
+        <p>hello</p>
+      </div>
     );
   }
 }
