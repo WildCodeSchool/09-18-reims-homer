4397844549f8fa4eb09ea3cd7bc4a11a1eb61b3c
Florentin C <florentin.coyard@gmail.com>
2018-12-10 16:55:53 +0100

    did a nice and lovely form

--- front/react-voyage/src/SignUp.js
+++ front/react-voyage/src/SignUp.js
@@ -1,23 +1,70 @@
 import React, { Component, Fragment } from 'react';
+import { TextField, Snackbar, Button, IconButton } from "@material-ui/core"
+import CloseIcon from "@material-ui/icons/Close"
+
+import "./App.css"
 
 class SignUp extends Component {
 
+  state = {
+    open: false,
+  };
 
+  handleClick = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    this.setState({ open: false });
+  };
   render() {
     const { state, handleInputChange, handleSubmit } = { ...this.props }
     return (
       <Fragment>
-        <div> nouveau composant </div>
-        <h1>{JSON.stringify(state, 1, 1)}</h1>
+        <div> Sign Up </div>
+        {/* <h1>{JSON.stringify(state, 1, 1)}</h1> */}
         <form onSubmit={((event) => handleSubmit(event))}>
-          <input onChange={((event) => handleInputChange(event))} type="email" name="email" />
-          <input onChange={((event) => handleInputChange(event))} type="password" name="password" />
-          <input onChange={((event) => handleInputChange(event))} type="password" name="passwordbis" />
-          <input onChange={((event) => handleInputChange(event))} type="text" name="name" />
-          <input onChange={((event) => handleInputChange(event))} type="text" name="lastname" />
-          <input type="submit" value="Soumettre" />
+          <TextField label="Email" onChange={((event) => handleInputChange(event))} type="email" name="email" />
+          <TextField label="Password" onChange={((event) => handleInputChange(event))} type="password" name="password" />
+          <TextField label="Password Bis" onChange={((event) => handleInputChange(event))} type="password" name="passwordbis" />
+          <TextField label="Name" onChange={((event) => handleInputChange(event))} type="text" name="name" />
+          <TextField label="LastName" onChange={((event) => handleInputChange(event))} type="text" name="lastname" />
+          <br />
+          <Button onClick={this.handleClick} type="submit" value="Soumettre">Valider</Button>
         </form>
-        <p>{state.flash}</p>
+
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+          }}
+          open={this.state.open}
+          autoHideDuration={6000}
+          onClose={this.handleClose}
+          ContentProps={{
+            'aria-describedby': 'message-id',
+          }}
+          message={<span id="message-id">{state.flash}</span>}
+          action={[
+            <Button key="undo" color="secondary" size="small" onClick={this.handleClose}>
+              UNDO
+            </Button>,
+            <IconButton
+              key="close"
+              aria-label="Close"
+              color="inherit"
+              onClick={this.handleClose}
+            >
+              <CloseIcon />
+            </IconButton>,
+          ]}
+        />
+
+
       </Fragment>
     );
   }
