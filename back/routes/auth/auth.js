0780162b3a9a1dcd6ae397e5b4e755954cf5dc08
Fabien Raymond <voortexxx@gmail.com>
2018-12-10 20:53:26 +0100

    commit back

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -8,7 +8,6 @@ router.use(bodyParser.json());
 
 router.post("/signup", function(req, res, next) {
   const formData = req.body;
-  console.log(formData);
   connection.query("INSERT INTO users SET ?", formData, err => {
     if (err) res.status(500).json({ flash: err.message });
     else res.status(200).json({ flash: "User has been signed up !" });
