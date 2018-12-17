85c295b58db5b2ab9047159391c2fe4ed1081161
Corentin DESOTOCOBET <corentin.dsc@gmail.com>
2018-12-17 15:56:42 +0100

    070 homer

--- front/homer-odyssey/src/App.js
+++ front/homer-odyssey/src/App.js
@@ -1,15 +1,65 @@
 import React, { Component } from "react";
 import "./App.css";
-import Email from "./SignUp";
+import SignUp from "./SignUp";
+import SignIn from "./SignIn";
+import Profile from "./Profile";
 import { Container } from "reactstrap";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: "#11cb5f" }
+  },
+  typography: { useNextVariants: true }
+});
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      flash: ""
+    };
+  }
+
   render() {
     return (
       <Container>
         <header className="text-center">Homer Odyssey</header>
-
-        <Email />
+        <MuiThemeProvider theme={theme}>
+          <Grid container alignItems="center" style={{ height: "100%" }}>
+            <Grid item xs={12}>
+              <Paper elevation={24} style={{ margin: 32 }}>
+                <Grid container alignItems="center" justify="center">
+                  <Grid item xs={12} sm={6}>
+                    <img
+                      src="http://images.innoveduc.fr/react_odyssey_homer/wildhomer.png"
+                      alt="Homer pics"
+                    />
+                  </Grid>
+                  <Grid
+                    item
+                    xs={12}
+                    sm={6}
+                    // style={{ border: "2px solid #c19a0b" }}
+                  >
+                    {/* <Profile /> */}
+                    <BrowserRouter>
+                      <Switch>
+                        <Route exact path="/" component={SignIn} />
+                        <Route path="/signup" component={SignUp} />
+                        <Route path="/signin" component={SignIn} />
+                        <Route path="/profile" component={Profile} />
+                      </Switch>
+                    </BrowserRouter>
+                  </Grid>
+                </Grid>
+              </Paper>
+            </Grid>
+          </Grid>
+        </MuiThemeProvider>
       </Container>
     );
   }
--- front/homer-odyssey/src/SignIn.js
+++ front/homer-odyssey/src/SignIn.js
@@ -6,52 +6,46 @@ import CloseIcon from "@material-ui/icons/Close";
 import Button from "@material-ui/core/Button";
 import { Link, withRouter } from "react-router-dom";
 
-class SignUp extends Component {
+class SignIn extends Component {
   constructor(props) {
     super(props);
     this.state = {
       email: "",
-      password: "",
       firstname: "",
       lastname: "",
-      confirmPassword: "",
+      password: "",
       flash: "",
       openFlash: false
     };
 
-    this.handleSubmitSignUp = this.handleSubmitSignUp.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   updateFormField = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
 
-  handleSubmitSignUp(e) {
+  handleSubmit(e) {
     e.preventDefault();
-    if (this.state.password === this.state.confirmPassword) {
-      fetch("/auth/signup", {
-        method: "POST",
-        headers: new Headers({
-          "Content-Type": "application/json"
-        }),
-        body: JSON.stringify({
-          email: this.state.email,
-          password: this.state.password,
-          firstname: this.state.firstname,
-          lastname: this.state.lastname
-        })
+
+    fetch("/auth/signin", {
+      method: "POST",
+      headers: new Headers({
+        "Content-Type": "application/json"
+      }),
+      body: JSON.stringify({
+        email: this.state.email,
+        password: this.state.password
       })
-        .then(res => res.json())
-        .then(res =>
-          this.setState({ flash: res.flash, open: true }, () => {
-            if (res.type === "success") {
-              this.props.history.push("/");
-            }
-          })
-        );
-    } else {
-      alert("Passwords don't match!!!!!");
-    }
+    })
+      .then(res => res.json())
+      .then(res =>
+        this.setState({ flash: res.flash, open: true }, () => {
+          if (res.type === "success") {
+            this.props.history.push("profile");
+          }
+        })
+      );
   }
 
   handleClose = (event, reason) => {
@@ -65,7 +59,7 @@ class SignUp extends Component {
   render() {
     return (
       <form>
-        <p>Inscription</p>
+        <p>Connexion</p>
         <TextField
           value={this.state.email}
           type="email"
@@ -79,31 +73,7 @@ class SignUp extends Component {
           name="password"
           placeholder="Your password"
           value={this.state.password}
-          onChange={event => this.updateFormField(event)}
-        />
-
-        <TextField
-          type="password"
-          name="confirmPassword"
-          placeholder="Confirm your password"
-          value={this.state.confirmPassword}
-          onChange={event => this.updateFormField(event)}
-        />
-
-        <TextField
-          type="firstname"
-          name="firstname"
-          placeholder="Your firstname"
-          value={this.state.firstname}
-          onChange={event => this.updateFormField(event)}
-        />
-
-        <TextField
-          type="lastname"
-          name="lastname"
-          placeholder="Your lastname"
-          value={this.state.lastname}
-          onChange={event => this.updateFormField(event)}
+          onChange={this.updateFormField}
         />
         <hr />
         <Button
@@ -111,14 +81,14 @@ class SignUp extends Component {
           variant="contained"
           type="submit"
           value="Soumettre"
-          onClick={e => this.handleSubmitSignUp(e)}
+          onClick={e => this.handleSubmit(e)}
         >
-          S'inscrire
+          Soumettre
         </Button>
 
         <hr />
-        <Link to="/signin">
-          <Button>Dèjà inscrit ?</Button>
+        <Link to="/signup">
+          <Button>Inscris toi !</Button>
         </Link>
         <Snackbar
           anchorOrigin={{
@@ -148,4 +118,4 @@ class SignUp extends Component {
   }
 }
 
-export default withRouter(SignUp);
+export default withRouter(SignIn);
