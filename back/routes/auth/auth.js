ada337c5c5c369556c59945cf6e81d662f69df22
Quentin Da Silva <quentin_51@live.fr>
2018-12-17 10:02:14 +0100

    070 - route

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -9,4 +9,12 @@ router.post("/signup", function(req, res, next) {
   });
 });
 
+router.post("/signin", function(req, res, next) {
+  const formData = req.body;
+  connection.query("INSERT INTO users SET ?", formData, (err, results) => {
+    if (err) res.status(500).json({ flash: err.message });
+    else res.status(200).json({ flash: "User has been signed up !" });
+  });
+});
+
 module.exports = router;
